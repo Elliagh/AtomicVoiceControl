38c95c1db77411922aa64400447b4970c3b59e96
elliagh <iviglbo@gmail.com>
2023-10-11 00:22:50 +0300

    sql not stable version

--- models/models.py
+++ models/models.py
@@ -1,4 +1,39 @@
+from datetime import datetime
+
 from sqlalchemy import MetaData, Integer, String, TIMESTAMP, ForeignKey, Table, Column
 
 metadata = MetaData()
 
+#TODO many to many, location converter
+users = Table(
+    "users",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("firstname", String),
+    Column("secondname", String),
+    Column("location", String, nullable=False),
+    Column("email", String, nullable=False),
+    Column("address", String),
+    Column("number", Integer),
+    Column("car_id", Integer, ForeignKey("cars.id"))
+)
+
+cars = Table(
+    "cars",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("location", String, nullable=False),
+    Column("user_id", Integer, ForeignKey("users.id")),
+    Column("sign", String),
+    Column("Battery", Integer),
+    Column("Type", String)
+)
+
+register = Table(
+    "register",
+    metadata,
+    Column("token", String),
+    Column("user_id", Integer, ForeignKey("users.id")),
+    Column("password", String, nullable=False),
+    Column("last_registration", TIMESTAMP, default=datetime.utcnow),
+)
