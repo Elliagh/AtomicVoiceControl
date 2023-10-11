f877a023ba3adb6819d3e95df32af0aa0f3ab425
Kostan <6PwCnSEPh2NHrEA>
2023-10-11 08:15:32 +0300

    add commands.txt

--- SpeechRecognize/testrec.py
+++ SpeechRecognize/testrec.py
@@ -1,7 +1,18 @@
 import Recognizer as r
 
+def check_command(command):
+    with open("Commands.txt",encoding="utf-8") as file_commands:
+        for line in file_commands.readlines():
+            line  = line.rstrip('\n')
+            if line == command:
+                return True
+                break
+        return False
+
 
 rc = r.Recognizer()
 
 text = rc.get_text()
-print(text)
+print(check_command(text))
+
+
