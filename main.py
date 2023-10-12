3fb1a7c8cba1c940a581e40dc375f62779ea8022
Kostan <6PwCnSEPh2NHrEA>
2023-10-12 08:44:03 +0300

    add stubs and handler exception micro

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import uvicorn
 from fastapi import FastAPI, APIRouter, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from SpeechRecognize import Recognizer
 from SpeechValidators import ValidatorZoneAndCommand as vzac, CommandValidator as cv
 from SpeechValidators import ZoneValidator as zv
@@ -24,7 +24,10 @@ async def get(request: Request):
 @app.get("/speak", response_class=HTMLResponse)
 def speakmicro(request: Request):
     rec = Recognizer.Recognizer()
-    text = rec.get_text()
+    try:
+        text = rec.get_text()
+    except:
+        return RedirectResponse("/")
     user_location = "40.7128 74.00601"
     return templates.TemplateResponse("RightCommandOrNot.html", {"request": request,
                                                                  "text": text, "user_location": user_location})
@@ -51,3 +54,61 @@ def send_command(request: Request, command: str, location_user: str):
 
 if __name__ == '__main__':
     uvicorn.run(app, host="localhost", port=8080)
+
+
+def light_on():
+    pass
+
+
+def light_off():
+    pass
+
+
+def near_battery_station():
+    pass
+
+def open_door():
+    pass
+
+def close_door():
+    pass
+
+def get_location_car():
+    pass
+
+def get_charge():
+    pass
+
+def on_near_light():
+    pass
+
+def off_near_light():
+    pass
+
+def on_far_light():
+    pass
+
+def off_far_light():
+    pass
+
+def build_route(point):
+    pass
+
+def open_trunk():
+    pass
+
+def close_trunk():
+    pass
+
+def on_conditioner():
+    pass
+
+def off_conditioner():
+    pass
+
+def open_hood():
+    pass
+
+def close_hood():
+    pass
+
